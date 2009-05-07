139ed3b00e65203c1a0553a11f6b5353d66b4265
alex94040 <alex94040@gmail.com>
2009-05-07 01:03:51 +0000

    Finishing up the move of drafts

--- includes/drafts/dashboard/PersonListPanel.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// This is the HTML template include file (.tpl.php) for PersonListPanel.
-	// Remember that this is a DRAFT.  It is MEANT to be altered/modified.
-	// Be sure to move this out of the drafts/dashboard directory before modifying to ensure that subsequent 
-	// code re-generations do not overwrite your changes.
-?>
-	<?php $_CONTROL->dtgPeople->Render(); ?>
-	<p><?php $_CONTROL->btnCreateNew->Render(); ?></p>
