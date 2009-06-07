9fbb6d9cbbb3abb8a03f555c908965352cea2b3e
alex94040 <alex94040@gmail.com>
2009-06-07 07:04:55 +0000

    QFolder::DeleteFolder function was removed by mistake.

--- includes/qcubed/_core/framework/QFolder.class.php
+++ includes/qcubed/_core/framework/QFolder.class.php
@@ -38,6 +38,34 @@ class QFolder {
 		return $blnReturn;
 	}
 	
+	/**
+	 * Allows for deletion of non-empty directories - takes care of
+	 * recursion appropriately.
+	 *
+	 * @return int number of deleted files
+	 */
+	public static function DeleteFolder($strPath) {
+		if (!is_dir($strPath)) {
+			unlink($strPath);
+			return 1;
+		}
+
+		$d = dir($strPath);
+		$count = 0;
+		while($entry = $d->read()) { 
+			if ($entry!= "." && $entry != "..") { 
+				if (is_dir($strPath)) {
+					$count += QFolder::DeleteFolder($strPath . "/" . $entry);
+				} 
+			} 
+		} 
+
+		$d->close(); 
+		rmdir($strPath);
+		
+		return $count;
+	}
+
 	public static function isWritable($strPath) {
 		if ($strPath[strlen($strPath) - 1] != "/") {
 			$strPath .= "/";
