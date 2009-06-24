a924593d6a9a7dfc0942ded0b810dfc9933c9050
marcosdsanchez <marcosdsanchez@thinkclear.com.ar>
2009-06-24 12:06:44 +0000

    fixes #279 in 1.0 branch. Thanks meetsing

--- wwwroot/examples/datagrid/sorting.tpl.php
+++ wwwroot/examples/datagrid/sorting.tpl.php
@@ -11,7 +11,7 @@
 		itself.<br/><br/>
 
 		So what you do is you specify the <b>QQ OrderBy Clause</b> that you would want run
-		for each column.  Then you pass the this clause to your class's <b>LoadAll</b> or <b>LoadArrayArrayByXXX</b> 
+		for each column.  Then you pass the this clause to your class's <b>LoadAll</b> or <b>LoadArrayByXXX</b> 
 		method as one of the optional <b>QQ Clause</b> parameters.  Note that all Qcubed code generated <b>LoadAll</b> and <b>LoadArrayByXXX</b>
 		methods take in an optional <b>$objOptionalClauses</b> parameter which conveniently uses the clause returned by the <b>QDataGrid</b>'s
 		<b>OrderByClause</b> method.<br/><br/>
