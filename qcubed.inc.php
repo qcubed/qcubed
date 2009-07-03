5152a145844a435e2cfeb32fec2326b51f3af1f1
alex94040 <alex94040@gmail.com>
2009-07-03 23:30:41 +0000

    Fixing a typo.

--- qcubed.inc.php
+++ qcubed.inc.php
@@ -1,6 +1,6 @@
 <?php 
 
-$includesPath = "/includes/configuration";
+$includesPath = "includes/configuration";
 
 if (isset($__CONFIG_ONLY__) && $__CONFIG_ONLY__ == true) {
 	require_once($includesPath . '/configuration.inc.php');
