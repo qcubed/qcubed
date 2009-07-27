dfda15f7e11f81cc05ea6d0379f2e26bd95ff1da
scottux <d.scott.carroll@gmail.com>
2009-07-27 18:03:18 +0000

    Fixed logo path

--- assets/_core/php/examples/includes/footer.inc.php
+++ assets/_core/php/examples/includes/footer.inc.php
@@ -1,6 +1,6 @@
 			</div>
 			<div id="footer">
-				<div id="footerLeft"><a href="http://qcu.be/" title="Visit the QCubed Homepage"><img src="<?php _p(__VIRTUAL_DIRECTORY__ . __IMAGE_ASSETS__ . '/images/qcubed_logo_footer.png'); ?>" alt="QCubed - A Rapid Prototyping PHP5 Framework" /></a></div>
+				<div id="footerLeft"><a href="http://qcu.be/" title="Visit the QCubed Homepage"><img src="<?php _p(__VIRTUAL_DIRECTORY__ . __IMAGE_ASSETS__ . '/qcubed_logo_footer.png'); ?>" alt="QCubed - A Rapid Prototyping PHP5 Framework" /></a></div>
 				<div id="footerRight">
 					<div><span class="footerSmall">For more information, please visit the QCubed website at <a href="http://www.qcu.be/" class="footerLink">http://www.qcu.be/</a></span></div>
 					<div><span class="footerSmall">Questions, comments, or issues can be discussed at the <a href="http://qcu.be/forum" class="footerLink">Examples Site Forum</a></span></div>
