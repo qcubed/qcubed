f2670c4111521e6bca5c9f3f5697aa9b169aeaf0
alex94040 <alex94040@gmail.com>
2009-10-09 20:54:43 +0000

    Fixing a merge issue in the last commit (class declared twice)

--- includes/qcubed/_core/framework/QDateTime52.class.php
+++ includes/qcubed/_core/framework/QDateTime52.class.php
@@ -10,15 +10,3 @@
 	}
 	
 	?>
-	<?php
-	
-	// PHP 5.3 and above does not allow for __toString() function to
-	// accept arguments. Previous versions of QCubed (including 1.0 and QCodo)
-	// allowed it. Preserving this for back-compat. 
-	class QDateTime extends QDateTimeBase {
-		public function __toString($strFormat) {
-			return parent::qFormat($strFormat);
-		}
-	}
-	
-	?>
