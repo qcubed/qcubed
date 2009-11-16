aa541447c478c7012da5b765378b7cf5a6524400
alex94040 <alex94040@gmail.com>
2009-11-16 19:07:13 +0000

    Fixes #376: PHP 5.3 Deprecated: Function set_magic_quotes_runtime().
    Follow-up to changelist [648] - improvements based on comments from Vexed.

--- includes/qcubed/_core/framework/DisableMagicQuotes.inc.php
+++ includes/qcubed/_core/framework/DisableMagicQuotes.inc.php
@@ -11,51 +11,51 @@ And modified to perfection */
 //PHP doesn't "magically" mangle backslashes!
 if (version_compare(PHP_VERSION, '5.3.0', '<')) {
 	set_magic_quotes_runtime(FALSE);
-}
 
-if (get_magic_quotes_gpc()) {
-	/*
-	All these global variables are slash-encoded by default,
-	because    magic_quotes_gpc is set by default!
-	(And magic_quotes_gpc affects more than just $_GET, $_POST, and $_COOKIE)
-	*/
-	global $HTTP_SERVER_VARS, $HTTP_GET_VARS,$HTTP_POST_VARS,$HTTP_COOKIE_VARS,$HTTP_POST_FILES,$HTTP_ENV_VARS;
-	//don't strip slashes from the remote user var
-	if(isset($_SERVER['REMOTE_USER']))
-		$remote_user = $_SERVER['REMOTE_USER'];
-	$_SERVER = stripslashes_array($_SERVER);
-	if(isset($_SERVER['REMOTE_USER']))
-		$_SERVER['REMOTE_USER'] = $remote_user;
-
-	$_GET = stripslashes_array($_GET);
-	$_POST = stripslashes_array($_POST);
-	$_COOKIE = stripslashes_array($_COOKIE);
-	//$_Files[*]['tmp_name'] is NOT escaped. Don't try to unescape it
-	$tmp_names = array();
-	foreach($_FILES as $key=>$file)
-		$tmp_names[$key] = $file['tmp_name'];
-	$_FILES = stripslashes_array($_FILES);
-	foreach($tmp_names as $key=>$file)
-		$_FILES[$key]['tmp_name'] = $file;
+	if (get_magic_quotes_gpc()) {
+		/*
+		All these global variables are slash-encoded by default,
+		because    magic_quotes_gpc is set by default!
+		(And magic_quotes_gpc affects more than just $_GET, $_POST, and $_COOKIE)
+		*/
+		global $HTTP_SERVER_VARS, $HTTP_GET_VARS,$HTTP_POST_VARS,$HTTP_COOKIE_VARS,$HTTP_POST_FILES,$HTTP_ENV_VARS;
+		//don't strip slashes from the remote user var
+		if(isset($_SERVER['REMOTE_USER']))
+			$remote_user = $_SERVER['REMOTE_USER'];
+		$_SERVER = stripslashes_array($_SERVER);
+		if(isset($_SERVER['REMOTE_USER']))
+			$_SERVER['REMOTE_USER'] = $remote_user;
 	
-	$_ENV = stripslashes_array($_ENV);
-	$_REQUEST = stripslashes_array($_REQUEST);
-	$HTTP_SERVER_VARS = stripslashes_array($HTTP_SERVER_VARS);
-	$HTTP_GET_VARS = stripslashes_array($HTTP_GET_VARS);
-	$HTTP_POST_VARS = stripslashes_array($HTTP_POST_VARS);
-	$HTTP_COOKIE_VARS = stripslashes_array($HTTP_COOKIE_VARS);
-	$HTTP_POST_FILES = stripslashes_array($HTTP_POST_FILES);
-	$HTTP_ENV_VARS = stripslashes_array($HTTP_ENV_VARS);
-	if (isset($_SESSION)) {    #These are unconfirmed (?)
-		$_SESSION = stripslashes_array($_SESSION, '');
-		$HTTP_SESSION_VARS = stripslashes_array($HTTP_SESSION_VARS, '');
+		$_GET = stripslashes_array($_GET);
+		$_POST = stripslashes_array($_POST);
+		$_COOKIE = stripslashes_array($_COOKIE);
+		//$_Files[*]['tmp_name'] is NOT escaped. Don't try to unescape it
+		$tmp_names = array();
+		foreach($_FILES as $key=>$file)
+			$tmp_names[$key] = $file['tmp_name'];
+		$_FILES = stripslashes_array($_FILES);
+		foreach($tmp_names as $key=>$file)
+			$_FILES[$key]['tmp_name'] = $file;
+		
+		$_ENV = stripslashes_array($_ENV);
+		$_REQUEST = stripslashes_array($_REQUEST);
+		$HTTP_SERVER_VARS = stripslashes_array($HTTP_SERVER_VARS);
+		$HTTP_GET_VARS = stripslashes_array($HTTP_GET_VARS);
+		$HTTP_POST_VARS = stripslashes_array($HTTP_POST_VARS);
+		$HTTP_COOKIE_VARS = stripslashes_array($HTTP_COOKIE_VARS);
+		$HTTP_POST_FILES = stripslashes_array($HTTP_POST_FILES);
+		$HTTP_ENV_VARS = stripslashes_array($HTTP_ENV_VARS);
+		if (isset($_SESSION)) {    #These are unconfirmed (?)
+			$_SESSION = stripslashes_array($_SESSION, '');
+			$HTTP_SESSION_VARS = stripslashes_array($HTTP_SESSION_VARS, '');
+		}
+		/*
+		The $GLOBALS array is also slash-encoded, but when all the above are
+		changed, $GLOBALS is updated to reflect those changes.  (Therefore
+		$GLOBALS should never be modified directly).  $GLOBALS also contains
+		infinite recursion, so it's dangerous...
+		*/
 	}
-	/*
-	The $GLOBALS array is also slash-encoded, but when all the above are
-	changed, $GLOBALS is updated to reflect those changes.  (Therefore
-	$GLOBALS should never be modified directly).  $GLOBALS also contains
-	infinite recursion, so it's dangerous...
-	*/
 }
 
 function stripslashes_array($data) {
