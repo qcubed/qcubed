026c6b1b24a2485b8e9857f08d091cf264134836
alex94040 <alex94040@gmail.com>
2009-11-16 19:01:48 +0000

    Fixes #378: QDateTime52.class.php has extra tab before php start tag
    This fix is for both 1.0 and 1.1 branches.

--- wwwroot/includes/qcodo/_core/framework/QDateTime52.class.php
+++ wwwroot/includes/qcodo/_core/framework/QDateTime52.class.php
@@ -1,12 +1,12 @@
-	<?php
-	
-	// PHP 5.3 and above does not allow for __toString() function to
-	// accept arguments. Previous versions of QCubed (including 1.0 and QCodo)
-	// allowed it. Preserving this for back-compat. 
-	class QDateTime extends QDateTimeBase {
-		public function __toString($strFormat = null) {
-			return parent::qFormat($strFormat);
-		}
+<?php
+
+// PHP 5.3 and above does not allow for __toString() function to
+// accept arguments. Previous versions of QCubed (including 1.0 and QCodo)
+// allowed it. Preserving this for back-compat. 
+class QDateTime extends QDateTimeBase {
+	public function __toString($strFormat = null) {
+		return parent::qFormat($strFormat);
 	}
-	
-	?>
+}
+
+?>
