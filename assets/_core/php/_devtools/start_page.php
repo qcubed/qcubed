173cf143551ad3942dca3c3a49cb4db607ddceff
scottux <d.scott.carroll@gmail.com>
2009-12-13 04:40:03 +0000

    Fixes Ticket #396

--- assets/_core/php/_devtools/start_page.php
+++ assets/_core/php/_devtools/start_page.php
@@ -34,7 +34,7 @@
 				<div class="code"><?php if (!QApplication::IsRemoteAdminSession()) QApplication::VarDump(); ?></div>
 			</div>
 			<div id="footer">
-				<div id="footerLeft"><a href="http://qcu.be/"><img src="<?php _p(__VIRTUAL_DIRECTORY__ . __IMAGE_ASSETS__ . '/images/qcubed_logo_footer.png'); ?>" alt="QCubed - A Rapid Prototyping PHP5 Framework" /></a></div>
+				<div id="footerLeft"><a href="http://qcu.be/"><img src="<?php _p(__VIRTUAL_DIRECTORY__ . __IMAGE_ASSETS__ . '/qcubed_logo_footer.png'); ?>" alt="QCubed - A Rapid Prototyping PHP5 Framework" /></a></div>
 				<div id="footerRight">
 					<div><span class="footerSmall">For more information, please visit the QCubed website at <a href="http://www.qcu.be/" class="footerLink">http://www.qcu.be/</a></span></div>
 					<div><span class="footerSmall">Questions, comments, or issues can be discussed at the <a href="http://qcu.be/forum" class="footerLink">Examples Site Forum</a></span></div>
