93216b5ba3683cc644e35731087d774ce2790685
VexedPanda <smite+qcubed@smite.ca>
2010-01-29 19:55:56 +0000

    Fixes #431

--- assets/_core/php/examples/datagrid/advanced_filtering.tpl.php
+++ assets/_core/php/examples/datagrid/advanced_filtering.tpl.php
@@ -3,21 +3,20 @@
 
 	<div class="instructions">
 		<h1 class="instruction_title">Adding More Complicated Filters to Your QDataGrid</h1>
-		This example will walk us through more advanced methods and types of filtering that can be done
+		<p>This example will walk us through more advanced methods and types of filtering that can be done
 		on your <b>QDataGrid</b>. Make sure you've got a good handle on the <a href="filtering.php">first 
-		example</a> before you dive into these shark-infested waters.<br/><br/>
+		example</a> before you dive into these shark-infested waters.</p>
 		
-		<h1>Changing MetaColumn Filters</h1>
-		It's worth noting that since MetaColumns will automatically set filters for you, you may have to
-		clear them first before applying new ones of your own configuration. To do so, just call the column's
-		FilterClear() function before setting up your new filter.
+		<h2>Changing MetaColumn Filters</h2>
+		<p>It's worth noting that since MetaColumns will automatically set filters for you, you may have to
+		clear them first before applying new ones of your own configuration. To do so, just set the column's
+		FilterType to None before setting up your new filter.</p>
 
 		<h2>Dropdown Lists</h2>
-		In <b>Example A's QDataGrid</b>, we've added a column filter that's a list. This is easily accomplished
-		using <b>FilterAddListItem</b> and setting the <b>FilterType</b> to <b>QFilterType::ListFilter</b>.
-		<br/><br/> 
+		<p>In <b>Example A's QDataGrid</b>, we've added a column filter that's a list. This is easily accomplished
+		using <b>FilterAddListItem</b> and setting the <b>FilterType</b> to <b>QFilterType::ListFilter</b>.</p>
 		
-		This example is filled from the Project type table, which is almost certainly small enough to avoid 
+		<p>This example is filled from the Project type table, which is almost certainly small enough to avoid 
 		any performance problems. You could theoretically do the same with the First Name column, by
 		populating the filter list with the entire collection of first names, but to do so you'd have to 
 		load all the people records into memory to build that list and then you're still forcing the user to 
@@ -25,28 +24,34 @@
 		benefits it had.
 		
 		<h2>Filter Constants</h2>
-		There are rare occasions where you need another filter to apply in addition to any the user supplies. 
+		<p>There are rare occasions where you need another filter to apply in addition to any the user supplies. 
 		So for example, you want to add a <b>QQ::Equal</b> filter that doesn't depend on the user's input 
-		into the filter textbox, but only when there's actually something supplied for that textbox.<br/><br/>
+		into the filter textbox, but only when there's actually something supplied for that textbox.</p>
 		
-		In <b>Example A</b>, the Last Name filter only considers users who also have an enabled log in.<br/><br/>
+		<p>In <b>Example A</b>, the Last Name filter only considers users who also have an enabled log in.</p>
 
 		<h2>Custom Filters</h2>
-		Filters aren't limited to just datagrids filled by using <b>QQuery</b> -- we've got support for custom 
+		<p>Filters aren't limited to just datagrids filled by using <b>QQuery</b> -- we've got support for custom 
 		filters as well. This will allow you to use filters on <b>QDataGrids</b> filled from arrays, including 
-		those created from custom SQL queries.<br/><br/>
+		those created from custom SQL queries.</p>
 		
-		If you aren't using <b>QQuery</b>, you must use the <b>FilterByCommand</b> property of the 
+		<p>If you aren't using <b>QQuery</b>, you must use the <b>FilterByCommand</b> property of the 
 		<b>QDataGridColumn</b> and the <b>FilterInfo</b> property of the <b>QDataGrid</b> instead of 
 		<b>Filter</b> and <b>Conditions</b> respectively. The <b>QDataGridColumn's FilterByCommand</b> property 
 		must take the form of an array. When retrieving the <b>QDataGrid's FilterByCommand</b> property, you 
 		will receive an array of the applied column's <b>FilterByCommands</b>, with the user's input set at 
-		each array's <b>'value'</b> key.<br/><br/>
+		each array's <b>'value'</b> key.</p>
 		
-		As a result you can pull up that same information during your <b>QDataGrid's Bind</b> function in 
-		order to perform the actual filtering on the columns the user has selected.<br/><br/>
+		<p>As a result you can pull up that same information during your <b>QDataGrid's Bind</b> function in 
+		order to perform the actual filtering on the columns the user has selected.</p>
 		
-		In <b>Example B</b>, we use raw SQL to allow a filter on an address count column.
+		<p>In <b>Example B</b>, we use raw SQL to allow a filter on an address count column.</p>
+		
+		<h2>Saving and Restoring Filter States</h2>
+		<p>If you want to remember what filters a user has applied to a datagrid, you can use the <b>GetFilters</b>
+		and <b>SetFilters</b> functions to do so. Simply call GetFilters every time the contents of the datagrid changes
+		(such as during the data bind), and store that somewhere (like the session). Then, when you want to re-create
+		that state (such as at Form_Create), use SetFilters to re-apply them.</p>
 	</div>
 
 	<h2>Example A</h2>
--- assets/_core/php/examples/other/meta_datagrids.tpl.php
+++ assets/_core/php/examples/other/meta_datagrids.tpl.php
@@ -3,27 +3,35 @@
 
 	<div class="instructions">
 		<h1 class="instruction_title">Generated DataGrid Objects</h1>
-		Although the <i>concept</i> is known as a <b>Meta</b> DataGrid... the generated <b>DataGrid</b> objects
+		<p>Although the <i>concept</i> is known as a <b>Meta</b> DataGrid... the generated <b>DataGrid</b> objects
 		in and of itself is just a subclass of the actual <b>QDataGrid</b> object.  (Note that this is different
 		than a <b>MetaControl</b>, which is <i>not</i> a control, but is in fact a single data object
-		and a collection of controls that can be generated from and linked to it.)<br/><br/>
+		and a collection of controls that can be generated from and linked to it.)</p>
 		
-		A generated/meta datagrid is simply a <b>QDataGrid</b> with a bunch of <b>Meta___()</b> methods to
-		allow you to easily define and add columns for a given data class.<br/><br/>
+		<p>A generated/meta datagrid is simply a <b>QDataGrid</b> with a bunch of <b>Meta___()</b> methods to
+		allow you to easily define and add columns for a given data class.</p>
 		
-		Using simple string properties or more complex (and more powerful) <b>QCubed Query Nodes</b>, you can
+		<p>Using simple string properties or more complex (and more powerful) <b>QCubed Query Nodes</b>, you can
 		add any column (even columns from linked tables) to the datagrid, and the meta-functionality
-		will automatically take care of things like the column's <b>Title</b>, <b>Html</b>, and <b>Sorting</b> properties.<br/><br/>
+		will automatically take care of things like the column's <b>Title</b>, <b>Html</b>, <b>Filter</b> and 
+		<b>Sorting</b> properties.</p>
 
-		It even comes with its own <b>MetaDataBinder()</b>, and the datagrid is already set up to use that
+		<p>It even comes with its own <b>MetaDataBinder()</b>, and the datagrid is already set up to use that
 		as its databinder (but of course, even this is override-able). It's also very easy to specify
 		a condition on the meta datagrid - you don't even need to define your own data bind function! Simply set
 		the <b>AdditionalConditions</b> property to an appropriate QQuery condition, and you're good to go. In
-		this example, we'll only show projects whose status is "open". <br/><br/>
+		this example, we'll only show projects whose status is "open". Clauses such as expand can also easily
+		be applied by similarly setting the AdditionalClauses property.</p>
 
-		But again, similar to <b>MetaControls</b>, note that the datagrid is just a regular <b>QDataGrid</b> object,
+		<p>But again, similar to <b>MetaControls</b>, note that the datagrid is just a regular <b>QDataGrid</b> object,
 		and the columns are just regular <b>QDataGridColumn</b> objects, which means that you can modify 
-		the colums or the datagrid itself however you see fit.
+		the colums or the datagrid itself however you see fit.</p>
+		
+		<p>For example, you can change or add a filter to a meta-created <b>QDataGridColumn</b>. Just remember
+		that if there's already one auto-generated for you, you may want to clear it first
+		by changing the <b>FilterType</b> to <b>None</b> before applying your new filter to it. This
+		is particularly important when switching to a <b>ListFilter</b> type from a <b>TextFilter</b> type
+		because the old text filter would otherwise show up as an entry in the dropdown labeled "0".</p>
 	</div>
 
 	<?php $this->dtgProjects->Render(); ?>
