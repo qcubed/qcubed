17c3204a81673171e70305e19ad9d7d83e5d996f
alex94040 <alex94040@gmail.com>
2010-02-11 00:10:38 +0000

    Fixed #134 (Fatal error on Firefox on a Mac). Code by tompalan, review/testing by alex94040. This checkin is for the 1.1 branch.

--- includes/qcubed/_core/base_controls/_events.inc.php
+++ includes/qcubed/_core/base_controls/_events.inc.php
@@ -160,7 +160,11 @@
 		class QDownArrowKeyEvent extends QKeyPressEvent {
 			protected $strCondition = 'event.keyCode == 40';
 		}
-	} else {
+	}
+	
+	if (!QApplication::IsBrowser(QBrowserType::Macintosh)) { 
+		// DO NOT CHANGE TO "else" 
+		// this construct is needed to fix the "redeclared QEnterKeyEvent"-bug with Mac-browsers 
 		class QEnterKeyEvent extends QKeyDownEvent {
 			protected $strCondition = 'event.keyCode == 13';
 		}
@@ -174,4 +178,5 @@
 			protected $strCondition = 'event.keyCode == 40';
 		}
 	}
+	
 ?>
