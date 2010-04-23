e7b780e2aab3c61873f1bbdf8cd43682727253c4
alex94040 <alex94040@gmail.com>
2010-04-23 23:30:53 +0000

    Fixes #439: Improve QCalendar - new properties. Code by laceja; review by alex94040 and VexedPanda.

--- includes/qcubed/_core/base_controls/QCalendar.class.php
+++ includes/qcubed/_core/base_controls/QCalendar.class.php
@@ -1,30 +1,155 @@
 <?php
-	/**
-	 * This file contains the QCalendar class.
-	 *
-	 * @package Controls
-	 */
+/**
+ * This file contains the QCalendar class.
+ *
+ * @package Controls
+ */
 
-	/**
-	 * This class will render an HTML Button.
-	 *
-	 * @package Controls
-	 *
-	 * @property string $CalendarImageSource
-	 */
-	class QCalendar extends QDateTimeTextBox {
-		protected $strJavaScripts = __JQUERY_EFFECTS__;
-		protected $strStyleSheets = __JQUERY_CSS__;
+/**
+ * This class will render a pop-up, modeless calendar control 
+ * that can be used to let the user pick a date.
+ *
+ * @package Controls
+ *
+ */
+class QCalendar extends QDateTimeTextBox {
+	protected $strJavaScripts = __JQUERY_EFFECTS__;
+	protected $strStyleSheets = __JQUERY_CSS__;
+	protected $strMinDate = null;
+	protected $strMaxDate = null;
+	protected $strDefDate = null;
+	protected $strFirstDay = null;
+	protected $strNumberOfMonths = null;
+	protected $blnAutoSize = false;
+	protected $blnGotoCurrent = false;
+	protected $blnIsRtl = false;
+	protected $blnModified = false;
+
+	public function Validate() {
+		return true;
+	}
+
+	public function GetControlHtml() {
+		$strToReturn = parent :: GetControlHtml();
 		
-		public function Validate() {return true;}
-		public function GetControlHtml() {
-			$strToReturn = parent::GetControlHtml();
-			QApplication::ExecuteJavaScript(sprintf('$j("#%s").datepicker({showButtonPanel: true, dateFormat: "M d yy" })', $this->strControlId));
-			
-			return $strToReturn;
-		}
-		public function AddAction($objEvent, $objAction) {
-			throw new QCallerException('QJQCalendar does not support custom events');
+		QApplication :: ExecuteJavaScript(
+			sprintf('jQuery("#%s").datepicker({showButtonPanel: true,  dateFormat: "M d yy"' . 
+				(($this->blnAutoSize) ? ', autoSize: true' : '') . 
+				(($this->strMinDate) ? ', minDate: ' . $this->strMinDate : '') . 
+				(($this->strMaxDate) ? ', maxDate: "' . $this->strMaxDate . '"' : '') . 
+				(($this->strDefDate) ? $this->strDefDate : '') . 
+				(($this->strFirstDay) ? $this->strFirstDay : '') . 
+				(($this->blnGotoCurrent) ? ', gotoCurrent: true' : '') . 
+				(($this->blnIsRtl) ? ', isRTL: true' : '') . 
+				(($this->strNumberOfMonths) ? $this->strNumberOfMonths : '') . 
+			'})', $this->strControlId));
+
+		return $strToReturn;
+	}
+
+	/////////////////////////
+	// Public Properties: GET
+	/////////////////////////
+	public function __get($strName) {
+		switch ($strName) {
+			case "MinDate" :
+				return $this->strMinDate;
+			case "MaxDate" :
+				return $this->strMaxDate;
+			case "DefDate" :
+				return $this->strDefDate;
+			case "FirstDay" :
+				return $this->strFirstDay;
+			case "blnGotoCurrent" :
+				return $this->blnGotoCurrent;
+			case "blnIsRtl" :
+				return $this->blnIsRtl;
+			case "NumberOfMonths" :
+				return $this->strNumberOfMonths;
+			case "blnAutoSize" :
+				return $this->blnAutoSize;
+			default :
+				try {
+					return parent :: __get($strName);
+				}
+				catch (QCallerException $objExc) {
+					$objExc->IncrementOffset();
+					throw $objExc;
+				}
 		}
 	}
+
+	/////////////////////////
+	// Public Properties: SET
+	/////////////////////////
+	public function __set($strName, $mixValue) {
+		$this->blnModified = true;
+		switch ($strName) {
+			case "MinDate" :
+				$blnMinDate = true;
+				if ($mixValue == "Today")
+					$this->strMinDate = "new Date()";
+				else
+					$this->strMinDate = "new Date(" . $mixValue . ")";
+				break;
+			case "MaxDate" :
+				$blnMaxDate = true;
+				$this->strMaxDate = "new MaxDate('" . $mixValue . "')";
+				break;
+			case "defaultDate" :
+				$blnDefDate = true;
+				$this->strDefDate = ", defaultDate: +" . $mixValue . " ";
+				break;
+			case "FirstDay" :
+				$blnFirstDay = true;
+				$this->strFirstDay = ", FirstDay: " . $mixValue;
+				break;
+			case "gotoCurrent" :
+				try {
+					$this->blnGotoCurrent = QType :: Cast($mixValue, QType :: Boolean);
+					break;
+				}
+				catch (QInvalidCastException $objExc) {
+					$objExc->IncrementOffset();
+					throw $objExc;
+				}
+			case "isRTL" :
+				try {
+					$this->blnIsRtl = QType :: Cast($mixValue, QType :: Boolean);
+					break;
+				}
+				catch (QInvalidCastException $objExc) {
+					$objExc->IncrementOffset();
+					throw $objExc;
+				}
+			case "NumberOfMonths" :
+				$blnNumberOfMonths = true;
+				$mixRowsCols = explode(',', $mixValue);
+				$this->strNumberOfMonths = ", NumberOfMonths: [" . $mixRowsCols[1] . ", " . $mixRowsCols[1] . "]";
+				break;
+			case "AutoSize" :
+				try {
+					$this->blnAutoSize = QType :: Cast($mixValue, QType :: Boolean);
+					break;
+				}
+				catch (QInvalidCastException $objExc) {
+					$objExc->IncrementOffset();
+					throw $objExc;
+				}
+			default :
+				try {
+					parent :: __set($strName, $mixValue);
+				}
+				catch (QCallerException $objExc) {
+					$objExc->IncrementOffset();
+					throw $objExc;
+				}
+				break;
+		}
+	}
+
+	public function AddAction($objEvent, $objAction) {
+		throw new QCallerException('QCalendar does not support custom events');
+	}
+}
 ?>
