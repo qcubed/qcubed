5064276b9ebfe1ac448f0892535721f720abc17e
basilieus <jon@upr.com>
2010-12-29 05:06:35 +0000

    Fixes #681 - Fixing tab issues on examples

--- assets/_core/php/examples/events_actions/event_propagation.php
+++ assets/_core/php/examples/events_actions/event_propagation.php
@@ -1,9 +1,9 @@
 <?php
-	require_once('../qcubed.inc.php');
+    require_once('../qcubed.inc.php');
 
-	class ExampleForm extends QForm {
+    class ExampleForm extends QForm {
 
-		protected $objPanel1;
+        protected $objPanel1;
         protected $objPanel2;
         protected $objPanel3;
         protected $objPanel4;
@@ -11,54 +11,54 @@
         protected function Form_Create() {
 
             /*
-             * These two panels here will demenstrate even bubbling
-             */
-			$this->objPanel1 = new QPanel($this);
-			$this->objPanel1->AutoRenderChildren = true;
-			$this->objPanel1->CssClass = 'container';
-			$this->objPanel1->Text = "I'm panel 1";
-			$this->objPanel1->AddAction(new QClickEvent(), new QAjaxAction('objPanel1_Click'));
+            * These two panels here will demenstrate even bubbling
+            */
+            $this->objPanel1 = new QPanel($this);
+            $this->objPanel1->AutoRenderChildren = true;
+            $this->objPanel1->CssClass = 'container';
+            $this->objPanel1->Text = "I'm panel 1";
+            $this->objPanel1->AddAction(new QClickEvent(), new QAjaxAction('objPanel1_Click'));
 
-			$this->objPanel2 = new QPanel($this->objPanel1);
-			$this->objPanel2->CssClass = 'container';
-			$this->objPanel2->Text = "I'm panel 2 and I'm a child of panel 1.
+            $this->objPanel2 = new QPanel($this->objPanel1);
+            $this->objPanel2->CssClass = 'container';
+            $this->objPanel2->Text = "I'm panel 2 and I'm a child of panel 1.
             <br/><br/>If you click me, both my click action and panel 1's click action will fire";
-			$this->objPanel2->AddAction(new QClickEvent(), new QAjaxAction('objPanel2_Click'));
+            $this->objPanel2->AddAction(new QClickEvent(), new QAjaxAction('objPanel2_Click'));
 
             /*
-             * These two panels here will demenstrate how to STOP even bubbling
-             */
-			$this->objPanel3 = new QPanel($this);
-			$this->objPanel3->CssClass = 'container';
-			$this->objPanel3->AutoRenderChildren = true;
-			$this->objPanel3->Text = "I'm panel 3";
-			$this->objPanel3->AddAction(new QClickEvent(), new QAjaxAction('objPanel3_Click'));
+            * These two panels here will demenstrate how to STOP even bubbling
+            */
+            $this->objPanel3 = new QPanel($this);
+            $this->objPanel3->CssClass = 'container';
+            $this->objPanel3->AutoRenderChildren = true;
+            $this->objPanel3->Text = "I'm panel 3";
+            $this->objPanel3->AddAction(new QClickEvent(), new QAjaxAction('objPanel3_Click'));
 
-			$this->objPanel4 = new QPanel($this->objPanel3);
-			$this->objPanel4->CssClass = 'container';
-			$this->objPanel4->Text = "I'm panel 4 and I'm a child of panel 3.
+            $this->objPanel4 = new QPanel($this->objPanel3);
+            $this->objPanel4->CssClass = 'container';
+            $this->objPanel4->Text = "I'm panel 4 and I'm a child of panel 3.
             <br/><br/>If you click me only my click action will fire thanks to QStopPropagationAction";
             // Note the addition of QStopPropagationAction()
-			$this->objPanel4->AddAction(new QClickEvent(), new QStopPropagationAction());
-			$this->objPanel4->AddAction(new QClickEvent(), new QAjaxAction('objPanel4_Click'));
+            $this->objPanel4->AddAction(new QClickEvent(), new QStopPropagationAction());
+            $this->objPanel4->AddAction(new QClickEvent(), new QAjaxAction('objPanel4_Click'));
 
-		}
+        }
 
-		public function objPanel1_Click($strFormId, $strControlId, $strParameter) {
-			 QApplication::DisplayAlert('Panel 1 Clicked');
-		}
+        public function objPanel1_Click($strFormId, $strControlId, $strParameter) {
+            QApplication::DisplayAlert('Panel 1 Clicked');
+        }
 
-		public function objPanel2_Click($strFormId, $strControlId, $strParameter) {
-			 QApplication::DisplayAlert('Panel 2 Clicked');
-		}
+        public function objPanel2_Click($strFormId, $strControlId, $strParameter) {
+            QApplication::DisplayAlert('Panel 2 Clicked');
+        }
 
-		public function objPanel3_Click($strFormId, $strControlId, $strParameter) {
-			 QApplication::DisplayAlert('Panel 3 Clicked');
-		}
+        public function objPanel3_Click($strFormId, $strControlId, $strParameter) {
+            QApplication::DisplayAlert('Panel 3 Clicked');
+        }
 
-		public function objPanel4_Click($strFormId, $strControlId, $strParameter) {
-			 QApplication::DisplayAlert('Panel 4 Clicked, panel 3 will not trigger a click');
-		}
+        public function objPanel4_Click($strFormId, $strControlId, $strParameter) {
+            QApplication::DisplayAlert('Panel 4 Clicked, panel 3 will not trigger a click');
+        }
 
     }
 
--- assets/_core/php/examples/events_actions/event_propagation.tpl.php
+++ assets/_core/php/examples/events_actions/event_propagation.tpl.php
@@ -1,15 +1,15 @@
 <?php require('../includes/header.inc.php'); ?>
-	<?php $this->RenderBegin(); ?>
+    <?php $this->RenderBegin(); ?>
 
-	<div class="instructions">
-		<h1 class="instruction_title">Event Propagation</h1>
-		Somtimes we want events to stop bubbling up the DOM tree.  To do this we use <b>QStopPropagationAction</b>.<br/><br/>
+    <div class="instructions">
+        <h1 class="instruction_title">Event Propagation</h1>
+        Somtimes we want events to stop bubbling up the DOM tree.  To do this we use <b>QStopPropagationAction</b>.<br/><br/>
 
-		Below are two examples.  The first example shows event bubbling.  The second example shows how to stop the bubbling effect using
+        Below are two examples.  The first example shows event bubbling.  The second example shows how to stop the bubbling effect using
         <b>QStopPropagationAction</b>.
-	</div>
+    </div>
 
-	<style>
+    <style>
         .container {
             padding: 20px;
             background-color: #f3f3f3;
@@ -17,12 +17,12 @@
         }
     </style>
 
-        <h5>Example with event bubbling</h5>
-        <?php $this->objPanel1->Render(); ?>
+    <h5>Example with event bubbling</h5>
+    <?php $this->objPanel1->Render(); ?>
 
 
-        <h5>Example without event bubbling</h5>
-        <?php $this->objPanel3->Render(); ?>
+    <h5>Example without event bubbling</h5>
+    <?php $this->objPanel3->Render(); ?>
 
-	<?php $this->RenderEnd(); ?>
+    <?php $this->RenderEnd(); ?>
 <?php require('../includes/footer.inc.php'); ?>
