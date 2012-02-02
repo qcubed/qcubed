32df3fc7a8a969ca846a51562b1915b06aed5fc8
Vardan Akopian <vakopian+github@gmail.com>
2012-02-02 16:20:51 +0000

    reviewed by alex. Fixes #764

--- includes/qcubed/_core/codegen/templates/db_orm/class_gen/json_methods.tpl.php
+++ includes/qcubed/_core/codegen/templates/db_orm/class_gen/json_methods.tpl.php
@@ -19,3 +19,19 @@
 		public function getJson() {
 			return json_encode($this->getIterator());
 		}
+
+		/**
+		 * Default "toJsObject" handler
+		 * Specifies how the object should be displayed in JQuery UI lists and menus. Note that these lists use
+		 * value and label differently.
+		 *
+		 * value 	= The short form of what to display in the list and selection.
+		 * label 	= [optional] If defined, is what is displayed in the menu
+		 * id 		= Primary key of object.
+		 *
+		 * @return an array that specifies how to display the object
+		 */
+		public function toJsObject () {
+			return JavaScriptHelper::toJsObject(array('value' => $this->__toString(), 'id' => <?php if ( count($objTable->PrimaryKeyColumnArray) == 1 ) { ?> $this-><?php echo $objTable->PrimaryKeyColumnArray[0]->VariableName  ?> <?php } ?><?php if ( count($objTable->PrimaryKeyColumnArray) > 1 ) { ?> array(<?php foreach ($objTable->PrimaryKeyColumnArray as $objColumn) { ?> $this-><?php echo $objColumn->VariableName  ?>, <?php } ?><?php GO_BACK(2); ?>) <?php } ?>));
+		}
+
