1f0edb9dff13b683c2e57d0a033780e00270e5e1
Vardan Akopian <vakopian+github@gmail.com>
2012-02-23 17:44:38 +0000

    code by sp, reviewed by vakopian. Fixes #790

--- includes/qcubed/_core/base_controls/QAccordionBase.class.php
+++ includes/qcubed/_core/base_controls/QAccordionBase.class.php
@@ -1,4 +1,31 @@
 <?php
+
+	/**
+	 * QAccordionBase
+	 * 
+	 * The QAccordionBase class defined here provides an interface between the generated
+	 * QAccordianGen class, and QCubed. This file is part of the core and will be overwritten
+	 * when you update QCubed. To override, make your changes to the QAccordion.class.php file instead.
+	 *
+	 * The Accordion decends from QPanel. There are a number of ways to create an Accordion, 
+	 * but the basics are that you put a series of block level items inside the Accordion (like divs, or h1, QPanels, etc.)
+	 * and it will automatically pick the first item as the header and the second item as the content that will be collapsed
+	 * or expanded, and will repeat that until the end of the Accordion block. 
+	 * 
+	 * If you want more control, you can assign a jQuery selector to the Header item and that selector 
+	 * will be used to find the headers within the Accordion. In this case, the next block level sibling to
+	 * the header will be used as the content for that header. For example, to use all the items with class ItemHeader
+	 * inside the Accordion panel as the headers for the accordion, do this:
+	 * 
+	 * $accordion->Header = '.ItemHeader';
+	 * 
+	 * To get or set the index of the item that is currently open, use the inherited ->Active value. 
+	 * 
+	 * The Accordion will generate a QChangeEvent when a new header is selected.
+	 * 
+	 * See the jQuery UI documentation for additional events, methods and options that may be useful.
+	 * 
+	 */
 	class QAccordionBase extends QAccordionGen
 	{
 		protected $blnAutoRenderChildren = true;
@@ -20,6 +47,39 @@
 				return null;
 			} else
 				return $strToReturn;
+		}
+		
+		// These functions are used to keep track of the selected index.
+		// To query or set the selected index, use ->Active
+		public function GetControlJavaScript() {
+			
+			$formId = $this->Form->FormId;
+			$strJS = parent::GetControlJavaScript();
+			
+			$strJS .=<<<FUNC
+			.on("accordionchange", function(event, ui) {
+			 			qcubed.recordControlModification("$this->ControlId", "SelectedIndex", ui.options.active);
+						qc.pA("$formId", "$this->ControlId", "QChangeEvent", "", "");
+			})						
+FUNC;
+			
+			return $strJS;
+		}
+				
+		public function __set($strName, $mixValue) {
+			$this->blnModified = true;
+			
+			switch ($strName) {
+				case 'SelectedIndex':
+					// Do not call this function, only for jQuery UI. Call ->Active or Activate() instead.
+					$this->mixActive = QType::Cast($mixValue, QType::Integer);	// will cause ->Active getter to always return index of content item that is currently active
+					break;
+
+				default:
+					parent::__set($strName, $mixValue);
+					break;
+			}
+			
 		}
 	}
 ?>
