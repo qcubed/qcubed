1de7731d1174087144db9fa3191774ef1ab1480c
Vardan Akopian <vakopian+github@gmail.com>
2012-04-05 00:35:23 +0000

    fixes #821

--- includes/qcubed/_core/_devtools_cli/cli_prepend.inc.php
+++ includes/qcubed/_core/_devtools_cli/cli_prepend.inc.php
@@ -2,7 +2,7 @@
 	// First, let's make sure that path_to_prepend.txt exists
 	$strPathToPrependTextFile = dirname(__FILE__) . '/path_to_prepend.txt';
 	if (!is_file($strPathToPrependTextFile))
-		exit("No path_to_prepend.txt file was found.\r\nPlease be sure to specify the absolute path to prepend.inc.php in the ./path_to_prepend.txt file!\r\n");
+		exit("Could not find ".dirname(__FILE__) . "/path_to_prepend.txt. No path_to_prepend.txt file was found.\r\nPlease be sure to specify the absolute path to prepend.inc.php in the ./path_to_prepend.txt file!\r\n");
 
 	// Next, use the absolute path found in path_to_prepend.txt
 	$strPathToPrepend = trim(file_get_contents($strPathToPrependTextFile));
@@ -16,14 +16,14 @@
 	else
 		exit("The prepend.inc.php file was not found at $strPathToPrepend.\r\nPlease be sre to specify the correct absolute path to prepend.inc.php in the ./path_to_prepend.txt file!\r\n");
 
-	// Finally, verify that __DEVTOOLS__ is configured correctly
-	if (!is_file(__DEVTOOLS__ . '/' . basename(__FILE__)))
-		exit("The __DEVTOOLS_ configuration constant in configuration.inc.php does not appear to be set correctly.\r\n");
-
-	$objStat1 = stat(__DEVTOOLS__ . '/' . basename(__FILE__));
-	$objStat2 = stat(__FILE__);
-	if ($objStat1['ino'] != $objStat2['ino'])
-		exit("The __DEVTOOLS__ configuration constant in configuration.inc.php does not appear to be set correctly [INode Mismatch].\r\n");
+//	// Finally, verify that __DEVTOOLS__ is configured correctly
+//	if (!is_file(__DEVTOOLS__ . '/' . basename(__FILE__)))
+//		exit("Could not find " . __DEVTOOLS__ . '/' . basename(__FILE__) . ". The __DEVTOOLS__ configuration constant in configuration.inc.php does not appear to be set correctly.\r\n");
+//
+//	$objStat1 = stat(__DOCROOT__ . __DEVTOOLS__ . '/' . basename(__FILE__));
+//	$objStat2 = stat(__FILE__);
+//	if ($objStat1['ino'] != $objStat2['ino'])
+//		exit("The __DEVTOOLS__ configuration constant in configuration.inc.php does not appear to be set correctly [INode Mismatch].\r\n");
 
 	// Finally, turn off output buffering
 	ob_end_flush();
