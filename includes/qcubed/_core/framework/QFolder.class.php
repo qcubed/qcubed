9b2210ccb430a8fa61f34c450c0c2171fe6abcf9
Vardan Akopian <Vardan_Akopian@dailyjournal.com>
2012-11-23 13:43:41 -0800

    fixes #864

--- includes/qcubed/_core/framework/QFolder.class.php
+++ includes/qcubed/_core/framework/QFolder.class.php
@@ -4,13 +4,12 @@ class QFolder {
 	/**
 	 * Same as mkdir but correctly implements directory recursion.
 	 * At its core, it will use the php MKDIR function.
-	 * 
 	 * This method does no special error handling.  If you want to use special error handlers,
 	 * be sure to set that up BEFORE calling MakeDirectory.
 	 *
-	 * @param 	string 	$strPath actual path of the directoy you want created
-	 * @param 	integer	$intMode optional mode
-	 * @return 	boolean	the return flag from mkdir
+	 * @param         string         $strPath actual path of the directoy you want created
+	 * @param         integer        $intMode optional mode
+	 * @return         boolean        the return flag from mkdir
 	 */
 	public static function MakeDirectory($strPath, $intMode = null) {
 		if (is_dir($strPath)) {
@@ -19,20 +18,24 @@ class QFolder {
 		}
 
 		// Check to make sure the parent(s) exist, or create if not
-		if (!QApplicationBase::MakeDirectory(dirname($strPath), $intMode)) {
+		if (!self::MakeDirectory(dirname($strPath), $intMode)) {
 			return false;
 		}
 
-		// Create the current node/directory, and return its result
-		$blnReturn = mkdir($strPath);
+		if (PHP_OS != "Linux") {
+			// Create the current node/directory, and return its result
+			$blnReturn = mkdir($strPath);
 
-		if ($blnReturn && !is_null($intMode)) {
-			// Manually CHMOD to $intMode (if applicable)
-			// mkdir doesn't do it for mac, and this will error on windows
-			// Therefore, ignore any errors that creep up
-			QApplication::SetErrorHandler(null);
-			chmod($strPath, $intMode);
-			QApplication::RestoreErrorHandler();
+			if ($blnReturn && !is_null($intMode)) {
+				// Manually CHMOD to $intMode (if applicable)
+				// mkdir doesn't do it for mac, and this will error on windows
+				// Therefore, ignore any errors that creep up
+				QApplication::SetErrorHandler(null);
+				chmod($strPath, $intMode);
+				QApplication::RestoreErrorHandler();
+			}
+		} else {
+			$blnReturn = mkdir($strPath, $intMode);
 		}
 
 		return $blnReturn;
