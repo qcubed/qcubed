f0ef36c896c01c51c039ad646910b64999dc6519
Scottux <d.scott.carroll@gmail.com>
2012-12-30 20:58:00 -0500

    further example styling, including the view source window and button

--- assets/_core/php/examples/includes/header.inc.php
+++ assets/_core/php/examples/includes/header.inc.php
@@ -5,17 +5,19 @@
 		<meta charset="<?php _p(QApplication::$EncodingType); ?>" />
 		<title><?php _p(Examples::PageName(), false); ?> - QCubed PHP 5 Development Framework - Examples</title>
 		<link rel="stylesheet" type="text/css" href="<?php _p(__VIRTUAL_DIRECTORY__ . __CSS_ASSETS__ . '/styles.css'); ?>"></link>
+		<link rel="stylesheet" type="text/css" href="<?php _p(__VIRTUAL_DIRECTORY__ . __EXAMPLES__ . '/includes/examples.css'); ?>"></link>
 	</head>
 	<body>
 		<header>
+			<div class="breadcrumb">
+<?php		if(!isset($mainPage)) { ?>
+				<span class="category-name"><?php _p((Examples::GetCategoryId() + 1) . '. ' . Examples::$Categories[Examples::GetCategoryId()]['name'], false); ?></span> / 
+<?php		} ?>
+				<strong class="page-name"><?php _p(Examples::PageName(), false); ?></strong>
+			</div>
+			
 <?php	if(!isset($mainPage)) { ?>
-			<span class="category-name"><?php _p((Examples::GetCategoryId() + 1) . '. ' . Examples::$Categories[Examples::GetCategoryId()]['name'], false); ?></span> / 
-<?php	} ?>
-			<span class="page-name"><?php _p(Examples::PageName(), false); ?></span>
-
-<?php	if(!isset($mainPage)) { ?>
-			<span class="page-links"><?php _p(Examples::PageLinks(), false); ?></span>
-			<button id="viewSource" onclick="javascript:ViewSource(<?php _p(Examples::GetCategoryId() . ',' . Examples::GetExampleId()); ?>);">View Source</button>
+			<nav class="page-links"><?php _p(Examples::PageLinks(), false); ?></nav>
 <?php	} ?>
 			
 		</header>
