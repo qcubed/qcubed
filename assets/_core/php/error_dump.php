3bd84b460897b3091510ac3c222c1c5c3a2a17d1
Scottux <d.scott.carroll@gmail.com>
2013-01-01 22:40:13 -0500

    error dump page

--- assets/_core/php/error_dump.php
+++ assets/_core/php/error_dump.php
@@ -1,29 +1,30 @@
 <?php
-	/*
-	 * error_page include file
-	 * 
-	 * expects the following variables to be set:
-	 *	$__exc_strType
-	 *	$__exc_strMessage
-	 *	$__exc_strObjectType
-	 *	$__exc_strFilename
-	 *	$__exc_intLineNumber
-	 *	$__exc_strStackTrace
-	 *
-	 * optional:
-	 *	$__exc_strRenderedPage
-	 *  $__exc_objErrorAttributeArray
-	 */
+/*
+ * error_page include file
+ *
+ * expects the following variables to be set:
+ * 	$__exc_strType
+ * 	$__exc_strMessage
+ * 	$__exc_strObjectType
+ * 	$__exc_strFilename
+ * 	$__exc_intLineNumber
+ * 	$__exc_strStackTrace
+ *
+ * optional:
+ * 	$__exc_strRenderedPage
+ *  $__exc_objErrorAttributeArray
+ */
 
-	$__exc_strMessageBody = htmlentities($__exc_strMessage);
-	$__exc_strMessageBody = str_replace(" ", "&nbsp;", str_replace("\n", "<br/>\n", $__exc_strMessageBody));
-	$__exc_strMessageBody = str_replace(":&nbsp;", ": ", $__exc_strMessageBody);
-	$__exc_objFileArray = file($__exc_strFilename);
+$__exc_strMessageBody = htmlentities($__exc_strMessage);
+$__exc_strMessageBody = str_replace(" ", "&nbsp;", str_replace("\n", "<br/>\n", $__exc_strMessageBody));
+$__exc_strMessageBody = str_replace(":&nbsp;", ": ", $__exc_strMessageBody);
+$__exc_objFileArray = file($__exc_strFilename);
 
-	header("HTTP/1.1 500 Internal Server Error");
+header("HTTP/1.1 500 Internal Server Error");
 ?>
+<!DOCTYPE html>
 <?php
-if(stristr($__exc_strMessage, "Invalid Form State Data") !== false) {
+if (stristr($__exc_strMessage, "Invalid Form State Data") !== false) {
 	// It was a invalid form state data
 	// We return this string because invalid form state data error response does not behave like other errors
 	// and gets unable to render the QDialogBox for the error. Since qcubed.js searches for '<html>' in the beginning
@@ -34,98 +35,60 @@ if(stristr($__exc_strMessage, "Invalid Form State Data") !== false) {
 <html>
 	<head>
 		<title>PHP <?php _p($__exc_strType); ?> - <?php _p($__exc_strMessage); ?></title>
-		<style>
-			body { font-family: 'Arial' 'Helvetica' 'sans-serif'; font-size: 11px; }
-			a:link, a:visited { text-decoration: none; }
-			a:hover { text-decoration: underline; }
-			pre { font-family: 'Lucida Console' 'Courier New' 'Courier' 'monospaced'; font-size: 11px; line-height: 13px; }
-			.page { padding: 10px; }
-			.headingLeft { background-color: #440066; color: #ffffff; padding: 10px 0px 10px 10px; font-family: 'Verdana' 'Arial' 'Helvetica' 'sans-serif'; font-size: 18px; font-weight: bold; width: 70%; vertical-align: middle; }
-			.headingLeftSmall { font-size: 10px; }
-			.headingRight { background-color: #440066; color: #ffffff; padding: 0px 10px 10px 10px; font-family: 'Verdana' 'Arial' 'Helvetica' 'sans-serif'; font-size: 10px; width: 30%; vertical-align: middle; text-align: right; }
-			.title { font-family: 'Verdana' 'Arial' 'Helvetica' 'sans-serif'; font-size: 19px; font-style: italic; color: #330055; }
-			.code { background-color: #f4eeff; padding: 1px 10px 1px 10px; }
-		</style>
-		<script type="text/javascript">
-			function ToggleHidden(strDiv) { var obj = document.getElementById(strDiv); var stlSection = obj.style; var isCollapsed = obj.style.display.length; if (isCollapsed) stlSection.display = ''; else stlSection.display = 'none'; }
-		</script>
+		<style type="text/css">@import url("<?php _p(__VIRTUAL_DIRECTORY__ . __CSS_ASSETS__); ?>/styles.css");</style>
+		<style>header { background: #780000; color: #fff; padding: 10px; }</style>
 	</head>
-	<body bgcolor="white" topmargin="0" leftmargin="0" marginheight="0" marginwidth="0">
-
-	<table border="0" cellspacing="0" width="100%">
-		<tr>
-			<td nowrap="nowrap" class="headingLeft"><span class="headingLeftSmall"><?php _p($__exc_strType); ?> in PHP Script<br /></span><?php _p($_SERVER["PHP_SELF"]); ?></div></td>
-			<td nowrap="nowrap" class="headingRight">
-				<b>PHP Version:</b> <?php _p(PHP_VERSION); ?>;&nbsp;&nbsp;<b>Zend Engine Version:</b> <?php _p(zend_version()); ?>;&nbsp;&nbsp;<b>QCubed Version:</b> <?php _p(QCUBED_VERSION); ?><br />
-				<?php if (array_key_exists('OS', $_SERVER)) printf('<b>Operating System:</b> %s;&nbsp;&nbsp;', $_SERVER['OS']); ?><b>Application:</b> <?php _p($_SERVER['SERVER_SOFTWARE']); ?>;&nbsp;&nbsp;<b>Server Name:</b> <?php _p($_SERVER['SERVER_NAME']); ?><br />
-				<b>HTTP User Agent:</b> <?php _p($_SERVER['HTTP_USER_AGENT']); ?></td>
-		</tr>
-	</table>
-
-	<div class="page">
-		<span class="title"><?php _p($__exc_strMessageBody, false); ?></span><br />
-			<b><?php _p($__exc_strType); ?> Type:</b>&nbsp;&nbsp;
-			<?php _p($__exc_strObjectType); ?>
-			<br /><br />
-
+	<body>
+		<header>
+			<span><?php _p($__exc_strType); ?> in PHP Script</span> <?php _p($_SERVER["PHP_SELF"]); ?>
+		</header>
+		<section id="content">
+			<h1><?php _p($__exc_strMessageBody, false); ?></h1>
+			<p><strong><?php _p($__exc_strType); ?> Type:</strong> <?php _p($__exc_strObjectType); ?></p>
 <?php
 			if (isset($__exc_strRenderedPage)) {
-				$_SESSION['RenderedPageForError'] = $__exc_strRenderedPage; ?>
-				<b>Rendered Page:</b>&nbsp;&nbsp;
-				<a href="<?php _p(__VIRTUAL_DIRECTORY__ . __PHP_ASSETS__) ;?>/error_already_rendered_page.php" target="_blank">
-					Click here</a> to view contents able to be rendered</a>
-				<br /><br />
+				$_SESSION['RenderedPageForError'] = $__exc_strRenderedPage;
+?>
+				<p><strong>Rendered Page:</strong>
+					<a target="_blank" href="<?php _p(__VIRTUAL_DIRECTORY__ . __PHP_ASSETS__); ?>/error_already_rendered_page.php">Click here</a> to view contents able to be rendered</a>
+				</p>
 <?php
 			}
 ?>
-			<b>Source File:</b>&nbsp;&nbsp;
-			<?php _p($__exc_strFilename); ?>
-			&nbsp;&nbsp;&nbsp;&nbsp;<b>Line:</b>&nbsp;&nbsp;
-			<?php _p($__exc_intLineNumber); ?>
-			<br /><br />
+			<p><strong>Source File:</strong> <?php _p($__exc_strFilename); ?> <strong>Line:</strong> <?php _p($__exc_intLineNumber); ?></p>
 
-			<div class="code">
+			<code><pre>
 <?php
-						_p('<pre>', false);
-						for ($__exc_intLine = max(1, $__exc_intLineNumber - 5); $__exc_intLine <= min(count($__exc_objFileArray), $__exc_intLineNumber + 5); $__exc_intLine++) {
-							if ($__exc_intLineNumber == $__exc_intLine)
-								printf("<font color=red>Line %s:    %s</font>", $__exc_intLine, htmlentities($__exc_objFileArray[$__exc_intLine - 1]));
-							else
-								printf("Line %s:    %s", $__exc_intLine, htmlentities($__exc_objFileArray[$__exc_intLine - 1]));
-						}
-						_p('</pre>', false);
+			for ($__exc_intLine = max(1, $__exc_intLineNumber - 5); $__exc_intLine <= min(count($__exc_objFileArray), $__exc_intLineNumber + 5); $__exc_intLine++) {
+				if ($__exc_intLineNumber == $__exc_intLine){
+					printf("<span class='warning'>Line %s:    %s</span>\n", $__exc_intLine, htmlentities($__exc_objFileArray[$__exc_intLine - 1]));
+				}else{
+					printf("Line %s:    %s\n", $__exc_intLine, htmlentities($__exc_objFileArray[$__exc_intLine - 1]));
+				}
+			}
 ?>
-			</div><br />
-
+			</pre></code>
 <?php
-			if (isset($__exc_objErrorAttributeArray))
+			if (isset($__exc_objErrorAttributeArray)) {
 				foreach ($__exc_objErrorAttributeArray as $__exc_objErrorAttribute) {
-					printf("<b>%s:</b>&nbsp;&nbsp;", $__exc_objErrorAttribute->Label);
+					printf("<p><strong>%s:</strong>&nbsp;&nbsp;", $__exc_objErrorAttribute->Label);
 					$__exc_strJavascriptLabel = str_replace(" ", "", $__exc_objErrorAttribute->Label);
 					if ($__exc_objErrorAttribute->MultiLine) {
-						printf("\n<a href=\"#\" onclick=\"ToggleHidden('%s'); return false;\">Show/Hide</a>",
-							$__exc_strJavascriptLabel);
-						printf('<br /><br /><div id="%s" class="code" style="Display: none;"><pre>%s</pre></div><br />',
-							$__exc_strJavascriptLabel,
-							htmlentities($__exc_objErrorAttribute->Contents));
-					} else
-						printf("%s\n<br /><br />\n", htmlentities($__exc_objErrorAttribute->Contents));
+						printf("\n<a href=\"#\" onclick=\"ToggleHidden('%s'); return false;\">Show/Hide</a></p>", $__exc_strJavascriptLabel);
+						printf('<code id="%s" style="display: none;"><pre>%s</pre></code>', $__exc_strJavascriptLabel, htmlentities($__exc_objErrorAttribute->Contents));
+					} else {
+						printf("%s</p>\n", htmlentities($__exc_objErrorAttribute->Contents));
+					}
 				}
+			}
 ?>
 
-			<b>Call Stack:</b>
-			<br><br>
-			<div class="code">
-				<pre><?php _p($__exc_strStackTrace); ?></pre>
-			</div><br />
+			<p><strong>Call Stack:</strong></p>
+			<code><pre><?php _p($__exc_strStackTrace); ?></pre></code>
 
-			<b>Variable Dump:</b>&nbsp;&nbsp;
-			<a href="#" onclick="ToggleHidden('VariableDump'); return false;">Show/Hide</a>
-			<br /><br />
-			<div id="VariableDump" class="code" style="Display: none;">
+			<p><strong>Variable Dump:</strong> <a href="#" onclick="ToggleHidden('VariableDump'); return false;">Show/Hide</a></p>
+			<code id="VariableDump" style="display: none;"><pre>
 <?php
-				_p('<pre>', false);
-
 				// Dump All Variables
 				foreach ($GLOBALS as $__exc_Key => $__exc_Value) {
 					// TODO: Figure out why this is so strange
@@ -156,7 +119,7 @@ if(stristr($__exc_strMessage, "Invalid Form State Data") !== false) {
 							else
 								$__exc_StrVarExport = htmlentities(var_export($__exc_ObjVariableArray[$__exc_Key], true));
 
-							$__exc_StrToDisplay .= sprintf("  <a href=\"#\" onclick=\"ToggleHidden('%s'); return false;\">%s</a>\n", $varCounter, $__exc_Key);
+							$__exc_StrToDisplay .= sprintf("  <a style='display:block' href='#%s' onclick='javascript:ToggleHidden(\"%s\"); return false;'>%s</a>\n", $varCounter, $varCounter, $__exc_Key);
 							$__exc_StrToDisplay .= sprintf("<div id=\"%s\" style='display:none'>%s</div>", $varCounter, $__exc_StrVarExport);
 							$varCounter++;
 						} catch (Exception $__exc_objExcOnVarDump) {
@@ -165,14 +128,28 @@ if(stristr($__exc_strMessage, "Invalid Form State Data") !== false) {
 					}
 				}
 
-				_p($__exc_StrToDisplay . '</pre>', false);
-				printf('<script type="text/javascript">%s</script>', $__exc_StrToScript);
+				_p($__exc_StrToDisplay, false);
 ?>
-			</div><br />
+			</pre></code>
+		</section>
 
-			<hr width="100%" size="1" color="#dddddd" />
-			<center><i><?php _p($__exc_strType); ?> Report Generated:&nbsp;&nbsp;<?php _p(date('l, F j Y, g:i:s A')); ?></i></center>
-		</font>
-	</div>
-	</body>
+		<hr />
+		
+		<p style="text-align: center;"><em><?php _p($__exc_strType); ?> Report Generated:&nbsp;<?php _p(date('l, F j Y, g:i:s A')); ?></em></p>
+		
+		<footer>
+			<strong>PHP Version:</strong> <?php _p(PHP_VERSION); ?>;&nbsp;<strong>Zend Engine Version:</strong> <?php _p(zend_version()); ?>;&nbsp;<strong>QCubed Version:</strong> <?php _p(QCUBED_VERSION); ?><br />
+			<?php if (array_key_exists('OS', $_SERVER)) printf('<strong>Operating System:</strong> %s;&nbsp;&nbsp;', $_SERVER['OS']); ?><strong>Application:</strong> <?php _p($_SERVER['SERVER_SOFTWARE']); ?>;&nbsp;<strong>Server Name:</strong> <?php _p($_SERVER['SERVER_NAME']); ?><br />
+			<strong>HTTP User Agent:</strong> <?php _p($_SERVER['HTTP_USER_AGENT']); ?>
+		</footer>
+	<?php printf('<script type="text/javascript">%s</script>', $__exc_StrToScript); ?>
+	<script type="text/javascript">
+		function ToggleHidden(strDiv) {
+			var obj = document.getElementById(strDiv);
+			var stlSection = obj.style;
+			var isCollapsed = obj.style.display.length;
+			if (isCollapsed) { stlSection.display = ''; }else{ stlSection.display = 'none'; }
+		}
+	</script>
+</body>
 </html>
