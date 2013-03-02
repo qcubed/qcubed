5a7938b800ea3a11fba740af2932abb0da7ad3fc
vaibhav <vaibhavkaushal123@gmail.com>
2013-03-03 03:28:22 +0530

    retry at documentation

--- includes/qcubed/controls/QJqRadioButton.class.php
+++ includes/qcubed/controls/QJqRadioButton.class.php
@@ -1,25 +1,20 @@
 <?php
 	/**
 	 * QJqRadioButton
-	 * 
+	 *
 	 * Put your customizations of the standard behavior here.
-	 * 
+	 *
 	 * @package Controls
-	 * @author 
-	 * @copyright 2011
-	 * @version $Id$
-	 * @access public
-	 */
-     
-	/**
-	 * @package Controls  
-	 * @author Qcubed
-	 * @copyright 
-	 * @version 2011
-	 * @access public
+	 * @filesource
 	 */
 
-	class QJqRadioButton extends QJqRadioButtonBase
-	{
+	/**
+	 * Implements a JQuery UI Radio Button
+	 *
+	 * It extends the QJqRadioButtonBase class. When writing your code, use this class instead of QJqRadioButtonBase.
+	 * This class can be modified by the developer to add functions and alter the already present functions.
+	 * @package Controls
+	 */
+
+	class QJqRadioButton extends QJqRadioButtonBase	{
 	}
-?>
