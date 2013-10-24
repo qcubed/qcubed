39f45843cfcd933939f9b738c31576a2055e6ce3
spekary <spekary@gmail.com>
2013-10-24 14:52:57 -0700

    Fixing autocomplete problem.

--- includes/base_controls/QAutocompleteBase.class.php
+++ includes/base_controls/QAutocompleteBase.class.php
@@ -269,7 +269,7 @@
 					break;
 
 				case 'MultipleValueDelimiter':
-					$a = $this->GetAllActions(QAutocomplete_SourceEvent);
+					$a = $this->GetAllActions('QAutocomplete_SourceEvent');
 					if (!empty ($a)) {
 						throw new Exception('Must set MultipleValueDelimiter BEFORE calling SetDataBinder');
 					}				
