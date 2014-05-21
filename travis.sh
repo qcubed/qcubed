6a51ad383092582154797c9c10aca3a4b19b4e50
spekary <spekary@gmail.com>
2014-05-21 11:49:40 -0700

    Adding comments

--- travis.sh
+++ travis.sh
@@ -1,6 +1,8 @@
-# Script to test the travis build.
-# Run this from DOCROOT
-# For example, from the default location, you would run vendor/qcubed/framework/travis.sh
+# Script to locally simulate a travis build from within your project's environment.
+# To use this:
+#	1) create a 'qcubed' database and run the corresponding examples script to load it with the example data
+# 	2) cd to your DOCROOT
+# 	3) run this script from the DOCROOT directory
+# For example, from the default root location after installation, you would run vendor/qcubed/framework/travis.sh
 export DB=mysql
-#echo `dirname $0`/travis/test.php
 `dirname $0`/travis/test.php `dirname $0`
