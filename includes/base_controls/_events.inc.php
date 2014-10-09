a8b043610a42ddd447dbc07df09619a71e782fe8
spekary <spekary@gmail.com>
2014-10-09 12:45:45 -0700

    More doc.

--- includes/base_controls/_events.inc.php
+++ includes/base_controls/_events.inc.php
@@ -244,7 +244,12 @@
 		protected $strCondition = 'event.keyCode == 8';
 	}
 
-	/** For textboxes. Something has been input into the textbox. Responds to cut/paste, search cancel, etc. Ignores arrow keys, etc. */
+	/**
+	 * Detects changes to textboxes and other input elements. Responds to cut/paste, search cancel, etc.
+	 * Ignores arrow keys, etc.
+	 * Not in IE8 or below. Buggy in IE9. Full support in IE10 and above.
+	 * No support in Safari 5 and below for textarea elements.
+	 */
 	class QInputEvent extends QEvent {
 		/** Event Name */
 		const EventName = 'input';
