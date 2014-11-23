7b68632935468760ecbc999affe75b8779625f5e
spekary <spekary@gmail.com>
2014-11-22 20:21:21 -0800

    Fixing comments in file to append

--- install/project/includes/app_includes/control_registry.inc.php
+++ install/project/includes/app_includes/control_registry.inc.php
@@ -4,21 +4,19 @@
  * Called by the MetaControl Designer to create a list of controls appropriate for the given database field type.
  * If you would like your own custom controls to be available to include in the metacontrol designer,
  * uncomment the appropriate line for the type of data your control edits, and put the name of your control in
- * the parentheses (this is a new way of specifying an array introduced in PHP 5.4)
- * The control will then be available in the list of controls that appear in the metacontrol desginer dialog
- * in the ControlClass entry.
+ * the quotes to add it to the appropriate array.
  */
 
-//$controls[QDatabaseFieldType::VarChar] = ['QTextBox'];
-//$controls[QDatabaseFieldType::Blob] = [];
-//$controls[QDatabaseFieldType::Char] = [];
-//$controls[QDatabaseFieldType::Integer] = [];
-//$controls[QDatabaseFieldType::Float] = [];
-//$controls[QDatabaseFieldType::Bit] = [];
-//$controls[QDatabaseFieldType::DateTime] = [];
-//$controls[QDatabaseFieldType::Date] = [];
-//$controls[QDatabaseFieldType::Time] = [];
-//$controls[QType::ArrayType] = []; // Many-to-one. Includes forward and unique reverse references.
-//$controls[QType::Association] = []; // Many-to-many.
+//$controls[QDatabaseFieldType::VarChar][] = 'QMyTextBox';
+//$controls[QDatabaseFieldType::Blob][] = '';
+//$controls[QDatabaseFieldType::Char][] = '';
+//$controls[QDatabaseFieldType::Integer][] = '';
+//$controls[QDatabaseFieldType::Float][] = '';
+//$controls[QDatabaseFieldType::Bit][] = '';
+//$controls[QDatabaseFieldType::DateTime][] = '';
+//$controls[QDatabaseFieldType::Date][] = '';
+//$controls[QDatabaseFieldType::Time][] = '';
+//$controls[QType::ArrayType][] = ''; // Many-to-one. Includes forward and unique reverse references.
+//$controls[QType::Association][] = ''; // Many-to-many.
 
 ?>
