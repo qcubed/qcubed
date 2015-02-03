48a34c3087f9a0b566195c3228ef075be102138c
spekary <spekary@gmail.com>
2015-02-02 18:23:57 -0800

    Action to toggle class on an object.

--- includes/base_controls/_actions.inc.php
+++ includes/base_controls/_actions.inc.php
@@ -1139,6 +1139,34 @@
 		}
 	}
 
+
+	/**
+	 * Toggles the given class on the objects identified by the given jQuery selector. If no selector given, then
+	 * the trigger control is toggled.
+	 *
+	 * @package Actions
+	 */
+	class QToggleCssClassAction extends QAction {
+		protected $strCssClass;
+		protected $strTargetSelector;
+
+		public function __construct($strCssClass, $strTargetSelector = null) {
+			$this->strCssClass = $strCssClass;
+			$this->strTargetSelector = $strTargetSelector;
+		}
+
+		public function RenderScript(QControl $objControl) {
+			// Specified a Temporary Css Class to use?
+			if ($this->strTargetSelector) {
+				$strSelector = $this->strTargetSelector;
+			} else {
+				$strSelector = '#' . $objControl->ControlId;
+			}
+			return sprintf("jQuery('%s').toggleClass('%s');", $strSelector, $this->strCssClass);
+		}
+	}
+
+
 	/**
 	 * Sets the CSS class of a control on the client side (does not update the server side)
 	 *
