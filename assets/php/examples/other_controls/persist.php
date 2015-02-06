15b6a00c5c57d9a55976576febffd844af4082a0
spekary <spekary@gmail.com>
2015-02-05 22:45:25 -0800

    Example

--- assets/php/examples/other_controls/persist.php
+++ assets/php/examples/other_controls/persist.php
@@ -2,25 +2,24 @@
 
 class PersistentExampleForm extends QForm {
 	// We will persist this control in the $_SESSION
-	protected $ddnProjectPicker;
+	protected $ddnProjectPicker1;
+	protected $ddnProjectPicker2;
 
-	// for the purpose of simplicity for this example,
-	// we'll create a public QLabel to pass in the status 
-	// information between the Persistent Control $ddnProjectPicker
-	// and this parent QForm. 
-	public $lblStatus; 
+	protected $btnReload;
 
 	protected function Form_Create() {
-		$this->lblStatus = new QLabel($this);		
-		// Initialize the text of the label; if the query
-		// gets executed, this will be overwritten
-		$this->lblStatus->Text = "The query to populate the dropdown was NOT executed";
 
-		$this->ddnProjectPicker = ProjectPickerListBox::CreatePersistent(
-			'ProjectPickerListBox', // name of the control class
-			$this, // parent - the current QForm
-			'ddnProjects' // id on the form - just your usual ControlID
-		);
+		$this->ddnProjectPicker1 = new ProjectPickerListBox ($this);
+		$this->ddnProjectPicker2 = new ProjectPickerListBox ($this);
+		$this->ddnProjectPicker2->SaveState = true;
+
+		$this->btnReload = new QButton($this);
+		$this->btnReload->Text = 'Reload the Page';
+		$this->btnReload->AddAction (new QClickEvent(), new QAjaxAction('btnReload_Click'));
+	}
+
+	protected function btnReload_Click() {
+		QApplication::Redirect('persist.php');
 	}
 }
 
@@ -28,28 +27,24 @@ class PersistentExampleForm extends QForm {
  * This class encapsulates the logic of populating a list box
  * with a set of projects.
  */
-class ProjectPickerListBox extends QListBox {            
-	
+class ProjectPickerListBox extends QListBox {
+
 	/**
 	 * This constructor will only be executed once - afterwards,
 	 * the state of the control will be stored into the $_SESSION
 	 * and, on future loads, populated from the session state.
 	 */
-	public function __construct($objParentObject, $strControlId) {
-		parent::__construct($objParentObject, $strControlId);
-				
+	public function __construct($objParentObject) {
+		parent::__construct($objParentObject);
+
 		$projects = Project::QueryArray(
-			QQ::All(),            
-			QQ::OrderBy(QQN::Project()->Name)                
+			QQ::All(),
+			QQ::OrderBy(QQN::Project()->Name)
 		);
-				
+
 		foreach ($projects as $project) {
 			$this->AddItem($project->Name, $project->Id);
 		}
-		
-		// Reset the status of the parent form's label to indicate
-		// that the query was actually run
-		$objParentObject->lblStatus->Text = "The query was executed";        
 	}
 }
 
