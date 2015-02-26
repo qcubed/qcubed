b6fcbae669a50fabea6243003a710de1f5e054d6
Vardan Akopian <vakopian@sustain.net>
2015-02-25 19:08:05 -0800

    fix some class autoloading

--- includes/qcubed.inc.php
+++ includes/qcubed.inc.php
@@ -248,19 +248,18 @@
 	QApplicationBase::$ClassFile['qmodelconnectoroptions'] = __QCUBED_CORE__ .'/codegen/QModelConnectorOptions.class.php';
 
 	QApplicationBase::$ClassFile['abstractcontrol_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/AbstractControl_CodeGenerator.class.php';
-	QApplicationBase::$ClassFile['qautocomplete_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QAutocomplete_CodeGenerator.class.php';
 	QApplicationBase::$ClassFile['qcheckbox_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QCheckBox_CodeGenerator.class.php';
 	QApplicationBase::$ClassFile['qcheckboxlist_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QCheckBoxList_CodeGenerator.class.php';
 	QApplicationBase::$ClassFile['qcontrol_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QControl_CodeGenerator.class.php';
-	QApplicationBase::$ClassFile['qdatepickerbox_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QDatepickerBox_CodeGenerator.class.php';
+	QApplicationBase::$ClassFile['qcsvtextbox_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QCsvTextBox_CodeGenerator.class.php';
 	QApplicationBase::$ClassFile['qdatetimepicker_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QDateTimePicker_CodeGenerator.class.php';
+	QApplicationBase::$ClassFile['qnumerictextbox_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QNumericTextBox_CodeGenerator.class.php';
 	QApplicationBase::$ClassFile['qfloattextbox_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QFloatTextBox_CodeGenerator.class.php';
 	QApplicationBase::$ClassFile['qintegertextbox_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QIntegerTextBox_CodeGenerator.class.php';
 	QApplicationBase::$ClassFile['qlabel_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QLabel_CodeGenerator.class.php';
 	QApplicationBase::$ClassFile['qlistbox_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QListBox_CodeGenerator.class.php';
 	QApplicationBase::$ClassFile['qlistcontrol_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QListControl_CodeGenerator.class.php';
 	QApplicationBase::$ClassFile['qradiobuttonlist_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QRadioButtonList_CodeGenerator.class.php';
-	QApplicationBase::$ClassFile['qslider_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QSlider_CodeGenerator.class.php';
 	QApplicationBase::$ClassFile['qtextbox_codegenerator'] = __QCUBED_CORE__ .'/codegen/controls/QTextBox_CodeGenerator.class.php';
 
 
