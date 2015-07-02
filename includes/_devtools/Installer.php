be4653972d333e0123a45aa9d4e76d51c52bb3d9
spekary <spekary@gmail.com>
2015-07-01 18:29:15 -0700

    Attempting to fix install warning after initial install.

--- includes/_devtools/Installer.php
+++ includes/_devtools/Installer.php
@@ -29,11 +29,6 @@ class Installer {
 			echo 'Copying ' . $strPackageName . " files.\n";
 			self::ComposerPluginInstall($strPackageName);
 		}
-		elseif (self::startsWith($strPackageName, 'qcubed/framework')) {
-			// First time installation of framework
-			echo 'Copying ' . $strPackageName . " files.\n";
-			self::ComposerFrameworkInstall($installedPackage->getExtra());
-		}
 	}
 
 	public static function postPackageUpdate(PackageEvent $event)
@@ -59,6 +54,23 @@ class Installer {
 			self::ComposerPluginUninstall($strPackageName);
 		}
 	}
+
+	public static function postRootInstall(PackageEvent $event)
+	{
+		$installedPackage = $event->getOperation()->getPackage();
+		$strPackageName = $installedPackage->getName();
+		if (self::startsWith($strPackageName, 'qcubed')) {	// double check that we are installing a qcubed project
+			self::ComposerFrameworkInstall($installedPackage->getExtra());
+
+			// add the scripts above to manage plugin installations and framework updates
+			$installedPackage->setScripts([
+				"post-package-install"=>["QCubed\\Devtools\\Installer::postPackageInstall"],
+				"post-package-update"=>["QCubed\\Devtools\\Installer::postPackageUpdate"],
+    			"post-package-uninstall"=>["QCubed\\Devtools\\Installer::postPackageUninstall"]
+			]);
+		}
+	}
+
 
 	/**
 	 * Move files out of the vendor directory and into the project directory that are in the plugin's install directory.
