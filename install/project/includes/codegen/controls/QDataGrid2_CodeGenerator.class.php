2c396237abe60f75591feab5c3cb00500b5e4ccd
spekary <spekary@gmail.com>
2015-10-16 09:56:56 -0700

    Fix for travis build.

--- install/project/includes/codegen/controls/QDataGrid2_CodeGenerator.class.php
+++ install/project/includes/codegen/controls/QDataGrid2_CodeGenerator.class.php
@@ -1,7 +1,6 @@
 <?php
 
-	//require_once(__QCUBED_CORE__ .'/codegen/controls/QDataGrid2Base_CodeGenerator.class.php');
-	require_once('QDataGrid2Base_CodeGenerator.class.php');
+	require_once(__QCUBED_CORE__ .'/codegen/controls/QDataGrid2Base_CodeGenerator.class.php');
 
 	class QDataGrid2_CodeGenerator extends QDataGrid2Base_CodeGenerator {
 	}
