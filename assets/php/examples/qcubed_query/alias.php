4d2e63b271427fd620a4f2493c9e66db74904e27
Oleg Abrosimov <olegabr@yandex.ru>
2016-01-10 13:08:06 +0600

    test and example for issue #439 Expand doesn't take Aliases into account
     - It is the same as the PR #441, but for beta-3.0 branch
     - The original fix #440 was wrong, but in beta-3.0 it is completely outdated.
     - This PR is to be sure that the original problem is not reproduced in the new code of v3.0

--- assets/php/examples/qcubed_query/alias.php
+++ assets/php/examples/qcubed_query/alias.php
@@ -53,6 +53,63 @@
 		_p($objProject->Name . " (" . $objProject->Description . ")");
 		_p('<br/>', false);
 	}
+?>
+
+	<h2>Example 3: Managers having one least a project with a conson milestone, and for each manager, the first voyel milestone and the first conson one</h2>
+<?php
+	$emptySelect = QQ::Select();
+	$emptySelect->SetSkipPrimaryKey(true);
+	$nVoyel = QQ::Alias(QQN::Person()->ProjectAsManager->Milestone, 'voyel');
+	$nConson = QQ::Alias(QQN::Person()->ProjectAsManager->Milestone, 'conson');
+	$objPersonArray = Person::QueryArray(
+		QQ::IsNotNull($nConson->Id),
+		QQ::Clause(
+			QQ::Expand(QQN::Person()->ProjectAsManager, null, $emptySelect),
+			QQ::Expand($nVoyel, QQ::In($nVoyel->Name, array('Milestone A', 'Milestone E', 'Milestone I')), $emptySelect),
+			QQ::Expand($nConson, QQ::NotIn($nConson->Name, array('Milestone A', 'Milestone E', 'Milestone I')), $emptySelect),
+			QQ::GroupBy(QQN::Person()->Id),
+			QQ::Minimum($nVoyel->Name, 'min_voyel'),
+			QQ::Minimum($nConson->Name, 'min_conson'),
+			//*** only needed in PG-SQL.
+			// Even with an empty select, id is selected;
+			// Happily, PG doesn't complain if both id and MIN(id) are selected
+			QQ::Expand(QQN::Person()->ProjectAsManager, null, $emptySelect),
+			QQ::Minimum(QQN::Person()->ProjectAsManager->Id, 'dummy'),
+			//***
+			QQ::Select(
+				QQN::Person()->FirstName,
+				QQN::Person()->LastName
+			)
+		)
+	);
+
+	foreach ($objPersonArray as $objManager){
+		_p($objManager->FirstName.' '.$objManager->LastName. " (" . $objManager->GetVirtualAttribute('min_voyel').', '. $objManager->GetVirtualAttribute('min_conson'). ")");
+		_p('<br/>', false);
+	}
+?>
+
+	<h2>Example 4: Projects with, for each one, the "min" city from the addresses containing 'r' and the "min" city from the addresses NOT containing 'r' </h2>
+<?php
+	$nWithR = QQ::Alias(QQN::Project()->PersonAsTeamMember->Person->Address, 'with_r');
+	$nWithoutR = QQ::Alias(QQN::Project()->PersonAsTeamMember->Person->Address, 'without_r');
+	$objProjectArray = Project::QueryArray(
+		QQ::All(),
+		QQ::Clause(
+			QQ::Expand($nWithR, QQ::Like($nWithR->Street, '%r%')),
+			QQ::Expand($nWithoutR, QQ::NotLike($nWithoutR->Street, '%r%')),
+			QQ::GroupBy(QQN::Project()->Id),
+			QQ::Minimum($nWithR->City, 'min_city_r'),
+			QQ::Minimum($nWithoutR->City, 'min_city_wor')
+		)
+	);
+
+	foreach ($objProjectArray as $objProject){
+		_p($objProject->Name . " (" . $objProject->GetVirtualAttribute('min_city_r').', '. $objProject->GetVirtualAttribute('min_city_wor'). ")");
+		_p('<br/>', false);
+	}
+
+
 	QApplication::$Database[1]->OutputProfiling();
 ?>
 </div>
