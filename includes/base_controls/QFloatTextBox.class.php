61e25767ebbb82b13b101395a8c80dbb4e42b75d
spekary <spekary@gmail.com>
2016-05-02 10:14:41 -0700

    There are ways to circumvent Validate, even accidentally. Overriding the Value attribute to return a sanitized version of the text box.

--- includes/base_controls/QFloatTextBox.class.php
+++ includes/base_controls/QFloatTextBox.class.php
@@ -9,7 +9,14 @@
 	 * A subclass of QNumericTextBox -- Validate will also ensure
 	 * that the Text is a valid float and (if applicable) is in the range of Minimum <= x <= Maximum
 	 *
+	 * We do not use the sanitize capability of QTextBox here. Sanitizing the data will change the data, and
+	 * if the user does not type in a valid float, we will not be able to put up a warning telling the user they made
+	 * a mistake. You can easily change this behavior by setting the following:
+	 * 	SanitizeFilter = FILTER_SANITIZE_NUMBER_FLOAT
+	 *  SanitizeFilterOptions = FILTER_FLAG_ALLOW_FRACTION
+	 *
 	 * @package Controls
+	 * @property int|null	 $Value			Returns the integer value of the text, sanitized.
 	 *
 	 */
 	class QFloatTextBox extends QNumericTextBox {
@@ -26,6 +33,25 @@
 			parent::__construct($objParentObject, $strControlId);
 			$this->strLabelForInvalid = QApplication::Translate('Invalid Float');
 			$this->strDataType = QType::Float;
+		}
+
+		public function __get($strName) {
+			switch ($strName) {
+				case "Value":
+					if ($this->strText === null || $this->strText === "") {
+						return null;
+					} else {
+						return (float)filter_var ($this->strText, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+					}
+
+				default:
+					try {
+						return parent::__get($strName);
+					} catch (QCallerException $objExc) {
+						$objExc->IncrementOffset();
+						throw $objExc;
+					}
+			}
 		}
 	}
 
--- includes/base_controls/QIntegerTextBox.class.php
+++ includes/base_controls/QIntegerTextBox.class.php
@@ -8,9 +8,13 @@
 	/**
 	 * A subclass of TextBox with its validate method overridden -- Validate will also ensure
 	 * that the Text is a valid integer and (if applicable) is in the range of Minimum <= x <= Maximum
+	 * 
+	 * We do not use the sanitize capability of QTextBox here. Sanitizing the data will change the data, and
+	 * if the user does not type in an integer, we will not be able to put up a warning telling the user they made
+	 * a mistake. You can easily change this behavior by setting SanitizeFilter = FILTER_SANITIZE_NUMBER_INT.
 	 *
 	 * @package Controls
-	 *
+	 * @property int|null	 $Value			Returns the integer value of the text, sanitized.
 	 */
 
 	class QIntegerTextBox extends QNumericTextBox {
@@ -28,6 +32,26 @@
 			$this->strLabelForInvalid = QApplication::Translate('Invalid Integer');
 			$this->strDataType = QType::Integer;
 		}
+
+		public function __get($strName) {
+			switch ($strName) {
+				case "Value": 
+					if ($this->strText === null || $this->strText === "") {
+						return null;
+					} else {
+						return (int)filter_var ($this->strText, FILTER_SANITIZE_NUMBER_INT);
+					}
+
+				default:
+					try {
+						return parent::__get($strName);
+					} catch (QCallerException $objExc) {
+						$objExc->IncrementOffset();
+						throw $objExc;
+					}
+			}
+		}
+
 	}
 
 ?>
