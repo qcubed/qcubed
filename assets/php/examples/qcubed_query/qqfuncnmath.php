c6ec2542834770c26ab0d9631a1f6a2b0ae0de95
spekary <spekary@gmail.com>
2016-05-08 22:56:53 -0700

    Fixing up the description of the example.

--- assets/php/examples/qcubed_query/qqfuncnmath.php
+++ assets/php/examples/qcubed_query/qqfuncnmath.php
@@ -4,25 +4,36 @@
 <div id="instructions">
 	<h1>SQL functions and math operations for QQuery</h1>
 
-	<p><a href="subsql.php">QQuery SubSql</a> is a very powerfull technique, but sometimes
-	you don't need so much control. In this case you can use QQuery Math Operations or SQL Functions</p>
+	<p>
+		While <a href="subsql.php">QQuery SubSql</a> gives you the ability to mix custom SQL queries with
+		QQuery queries, you can also build complex queries using the math and function QQuery statements without
+		needing to drop into custom SQL.</p>
 
-	<p>Math operations are: 
-	<strong>QQ::Add</strong> for addition (+), 
-	<strong>QQ::Sub</strong> for subtraction (-), 
-	<strong>QQ::Mul</strong> for multiplication (*), 
-	<strong>QQ::Div</strong> for division (/), 
-	and a <strong>QQ::MathOp</strong> to apply an arbitrary math operation to 2 or more operands</p>
+	<p>The standard SQL math operations included are: </p>
+	<ul>
+		<li><strong>QQ::Add</strong> for addition (+),</li>
+		<li><strong>QQ::Sub</strong> for subtraction (-),</li>
+		<li><strong>QQ::Mul</strong> for multiplication (*),</li>
+		<li><strong>QQ::Div</strong> for division (/),</li>
+	</ul>
 
-	<p>Exactly in the same way can be used SQL functions like: 
-	<strong>QQ::Abs</strong> for the absolute value (ABS), 
-	<strong>QQ::Ceil</strong> for the smallest integer value not less than the argument (CEIL), 
-	<strong>QQ::Floor</strong> for largest integer value not greater than the argument (FLOOR), 
-	<strong>QQ::Mod</strong> for the remainder (MOD), 
-	<strong>QQ::Power</strong> for the argument raised to the specified power (POWER), 
-	<strong>QQ::Sqrt</strong> for the square root of the argument (SQRT), 
-	and a <strong>QQ::Func</strong> to apply an arbitrary scalar function using the given parameters</p>
+	<p>You can also use  <strong>QQ::MathOp</strong> to apply any math operator that your particular flavor of SQL might
+		provide. For example, you can use QQ::MathOp to execute a bitwise shift operation ("&lt;&lt;") if you are using
+		MySQL or Postgres, even though that operator is not included in the SQL standard.</li>
+	</p>
 
+
+	<p>Similarly you can get the results of standard SQL functions, like:
+	<ul>
+	<li><strong>QQ::Abs</strong> for the absolute value (ABS), </li>
+	<li><strong>QQ::Ceil</strong> for the smallest integer value not less than the argument (CEIL), </li>
+	<li><strong>QQ::Floor</strong> for largest integer value not greater than the argument (FLOOR), </li>
+	<li><strong>QQ::Mod</strong> for the remainder (MOD), </li>
+	<li><strong>QQ::Power</strong> for the argument raised to the specified power (POWER), </li>
+	<li><strong>QQ::Sqrt</strong> for the square root of the argument (SQRT), </li>
+	</ul>
+	<p>And, you can use <strong>QQ::Func</strong> to get the results of any SQL function that your particular flavor of
+	SQL provides.</p>
 </div>
 
 <div id="demoZone">
@@ -42,7 +53,7 @@
 ?>
 	<p><?php QApplication::$Database[1]->OutputProfiling(); ?></p>
 
-	<h2>The same as above, but add calculated virtual field and do order by with it</h2>
+	<h2>The same as above, but also create a calculated virtual field and sort with it</h2>
 <?php
 
 	QApplication::$Database[1]->EnableProfiling();
@@ -70,8 +81,8 @@
 ?>
 	<p><?php QApplication::$Database[1]->OutputProfiling(); ?></p>
 
-	<h2>The same as above, but filter out all other fields with Select clause</h2>
-	<p>It also demonstrates the use of QQ::MathOp function</p>
+	<h2>The same as above and filter out most of the other fields by using a Select clause</h2>
+	<p>This also demonstrates how to use the QQ::MathOp function. </p>
 <?php
 
 	QApplication::$Database[1]->EnableProfiling();
@@ -105,8 +116,8 @@
 ?>
 	<p><?php QApplication::$Database[1]->OutputProfiling(); ?></p>
 
-	<h2>SQL Function usage</h2>
-	<p>It uses QQ::Abs function to retrieve not only overdue, but also, an opposite to an overdue projects</p>
+	<h2>SQL Function Example</h2>
+	<p>Use the QQ::Abs and QQ::Sub functions to retrieve projects both over-budget and under-budget by $20.</p>
 <?php
 
 	QApplication::$Database[1]->EnableProfiling();
@@ -122,7 +133,7 @@
 			, 20
 		),
 		QQ::Clause(
-			/* The most overdue first */
+			/* The most over budget first */
 			QQ::OrderBy(QQ::Virtual('absdiff'), 'DESC')
 			/* Required to access this field with GetVirtualAttribute */
 			, QQ::Expand(QQ::Virtual('absdiff'))
