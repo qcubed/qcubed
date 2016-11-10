5b86d7497aa7b0bf89c4c1b4cfac48ceb70a7ca5
spekary <spekary@gmail.com>
2016-11-09 17:02:38 -0800

    Additional fixes

--- includes/codegen/templates/db_orm/class_gen/object_reload.tpl.php
+++ includes/codegen/templates/db_orm/class_gen/object_reload.tpl.php
@@ -16,14 +16,15 @@
 		$this->__blnDirty = null;
 
 		// Reload the Object
-		$objReloaded = <?= $objTable->ClassName ?>::Load(<?php foreach ($objTable->PrimaryKeyColumnArray as $objColumn) { ?>$this-><?= $objColumn->VariableName ?>, <?php } ?><?php GO_BACK(2); ?>, $clauses);
+		$objReloaded = <?= $objTable->ClassName ?>::Load(<?php foreach ($objTable->PrimaryKeyColumnArray as $objColumn) { ?>$this-><?= $objColumn->Identity ? '' : '__' ?><?= $objColumn->VariableName ?>, <?php } ?><?php GO_BACK(2); ?>, $clauses);
 
 		// Update $this's local variables to match
 <?php foreach ($objTable->ColumnArray as $objColumn) { ?>
-<?php 	if ($objColumn->Identity) { ?>
+<?php 	if ($objColumn->Identity) { // implies primary key too ?>
 		$this->__blnValid[self::<?= strtoupper($objColumn->Name) ?>_FIELD] = true;
 <?php 	} elseif ($objColumn->PrimaryKey) { ?>
-		$this->__<?= $objColumn->VariableName ?> = $this-><?= $objColumn->VariableName ?>;
+		$this-><?= $objColumn->VariableName ?> = $objReloaded-><?= $objColumn->VariableName ?>;
+		$this->__blnValid[self::<?= strtoupper($objColumn->Name) ?>_FIELD] = true;
 <?php 	} elseif ($objColumn->Reference) { ?>
 		if (isset($objReloaded->__blnValid[self::<?= strtoupper($objColumn->Name) ?>_FIELD])) {
 <?php 	 	if ($objColumn->Reference->IsType) { ?>
